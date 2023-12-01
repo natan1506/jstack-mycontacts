3a254f4ff7c5e309f240fbd9aa6dbcafcce641fa
Natan Souza <42328139+natan1506@users.noreply.github.com>
2023-12-01 10:59:22 -0300

    Feat: create contacts controller

---
+++ backend/src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(req, res) {
+    res.send('Send Contact Controller');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
